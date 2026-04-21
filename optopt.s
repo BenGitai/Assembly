d909259ebdbfc667d96a07407bbb23008f2901bc
Jeremy Arking <ja8354@princeton.edu>
2026-04-21 09:16:56 -0400

    use caller saved

--- optopt.s
+++ optopt.s
@@ -14,15 +14,15 @@
     
    .global BigInt_add
     // stack variables
-    .equ ADD_STACK_BYTECOUNT, 80
+    .equ ADD_STACK_BYTECOUNT, 64
     LENGTH1 .req x19
     PDIGITS1 .req x20
     LENGTH2 .req x21
     PDIGITS2 .req x22
     LENGTH3 .req x23
     PDIGITS3 .req x24
-    ULSUM .req x25
-    LINDEX .req x26
+    ULSUM .req x17
+    LINDEX .req x18
     LSUMLENGTH .req x27
 
 BigInt_add:
@@ -37,9 +37,7 @@ BigInt_add:
         str PDIGITS2, [sp, 32]
         str LENGTH3, [sp, 40]
         str PDIGITS3, [sp, 48]
-        str ULSUM, [sp, 56]
-        str LINDEX, [sp, 64]
-        str LSUMLENGTH, [sp, 72]
+        str LSUMLENGTH, [sp, 56]
 
         // put params into callee saved registers
         ldr x3, [x0]
@@ -79,26 +77,24 @@ BigInt_add:
         // ulCarry = 0
         adds LINDEX, xzr, xzr
         cbz LSUMLENGTH, endLoop
-        lsl LSUMLENGTH, LSUMLENGTH, 3
         beginLoop:
         // body of for loop 
         // ulSum += oAddend1->aulDigits[lIndex];
-        ldr x0, [PDIGITS1, LINDEX]
+        ldr x0, [PDIGITS1, LINDEX, lsl 3]
         // ulSum += oAddend2->aulDigits[lIndex];
-        ldr x1, [PDIGITS2, LINDEX]
+        ldr x1, [PDIGITS2, LINDEX, lsl 3]
         
         adcs ULSUM, x0, x1
         // oSum->aulDigits[lIndex] = ulSum;
-        str ULSUM, [PDIGITS3, LINDEX]
+        str ULSUM, [PDIGITS3, LINDEX, lsl 3]
 
         // update loop variable
-        add LINDEX, LINDEX, 8
+        add LINDEX, LINDEX, 1
         // if (lIndex < lSumLength)
         sub x0, LSUMLENGTH, LINDEX
         cbz x0, endLoop
         b beginLoop
         endLoop:
-        lsr LSUMLENGTH, LSUMLENGTH, 3
         //if (ulCarry != 1) goto ulCarrynot1;
         adc x0, xzr, xzr
         cmp x0, 1
@@ -145,9 +141,7 @@ BigInt_add:
         ldr PDIGITS2, [sp, 32]
         ldr LENGTH3, [sp, 40]
         ldr PDIGITS3, [sp, 48]
-        ldr ULSUM, [sp, 56]
-        ldr LINDEX, [sp, 64]
-        ldr LSUMLENGTH, [sp, 72]
+        ldr LSUMLENGTH, [sp, 56]
         
         // restore stack frame
         ldr x30, [sp]
